d6cd75d5e1649d5c9752b159eef48a0caa12196f
Amit <amitkumarbca1990@gmail.com>
2023-03-13 19:06:14 +0530

    Files Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import Login from './pages/Login';
 import './App.css';
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,38 @@
 import React from "react";
-
+import Axios from 'axios';
+const formWrapper = {
+    width: "360px",
+}
 const Login = () =>{
     return(
-<div>
+<div className="container mt-5">
+<div className="loginPage d-flex aligns-items-center justify-content-center">
+   
+    <div className="formWrapper" style={formWrapper}>
     <h3>Login Page</h3>
+
+    <form>
+    <div className="form-floating mb-3">
+  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+  <label for="floatingInput">Email address</label>
+</div>
+<div className="form-floating">
+  <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+  <label for="floatingPassword">Password</label>
+</div>
+
+<div className="d-grid gap-2 mt-3">
+            <input type="submit" className="btn btn-primary" value="Login" />
+              
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+
+          </form>
+
+</div>
+</div>
 </div>
     );
 }
